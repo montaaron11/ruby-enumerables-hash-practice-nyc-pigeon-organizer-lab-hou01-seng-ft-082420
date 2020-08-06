212390c69042d8880fb3dbc0251d3a56a870cd41
Aaron Montgomery <montaaron11@gmail.com>
2020-08-06 04:39:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 def nyc_pigeon_organizer(data)
- hash.each_with_object({}) do |(k, v), a|
-   a[k] = v+ 3
+ data.each_with_object({}) do |(key, value), final_array|
+   final_array
  end 
  binding.pry 
 end
